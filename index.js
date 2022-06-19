752cba92d164d41266ae3ffba4a795058b9e8d31
JuliannaRkg544 <juliana.rkg544@gmail.com>
2022-06-19 12:42:01 -0300

    alguns dos bônus feitos

--- index.js
+++ index.js
@@ -8,20 +8,29 @@ server.use(cors()) //rever pra que serve
 let users = [];
 let tweets = [];
 
-server.listen(4000, ()=>{
+server.listen(5000, ()=>{
     console.log("servidor no ar");
 })
 
 server.post("/sign-up", (req,res)=>{
   const {username, avatar} = req.body;
+  if (!username || !avatar || typeof username != "string" || typeof avatar != "string"){
+      res.status(400).send("Todos os campos são obrigatórios!");
+      return;
+    }
   users.push({username, avatar});
-  res.send("OK");
+  res.send("OK").status(201) ;
 })
 
 server.post("/tweets", (req,res)=>{
-    const {username, tweet} = req.body;
-    tweets.push({username,tweet});
-    res.send("OK");
+    const {tweet} = req.body;
+    const Username = req.headers.user
+    if (!tweet || typeof tweet != "string"){
+        res.status(400).send("Todos os campos são obrigatórios!");
+        return;
+      }
+    tweets.push({Username,tweet});
+    res.status(201).send("OK");
 })
 
 server.get("/tweets", (req,res)=>{
@@ -31,3 +40,10 @@ server.get("/tweets", (req,res)=>{
 
     res.send(lastTweets);
 })
+
+// server.get("/tweets/:usernameID", (req,res)=>{
+//   const usernameID = req.params.usernameID
+//   console.log(usernameID)
+//   res.send(tweets.filter(tweet => tweet.Username == usernameID ));
+
+// })
